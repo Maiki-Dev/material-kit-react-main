f0b348c1d1fe20af3fc6dbe748750294d3191161
Neok <nozum1ke@gmail.com>
2023-05-01 04:51:55 +0800

    first commit

---
+++ src/sections/account/account-profile-details.js
@@ -0,0 +1,179 @@
+import { useCallback, useState } from 'react';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Divider,
+  TextField,
+  Unstable_Grid2 as Grid
+} from '@mui/material';
+
+const states = [
+  {
+    value: 'alabama',
+    label: 'Han-uul'
+  },
+  {
+    value: 'new-york',
+    label: 'Bayngol'
+  },
+  {
+    value: 'san-francisco',
+    label: 'Baynzurkh'
+  },
+  {
+    value: 'los-angeles',
+    label: 'Test'
+  }
+];
+
+export const AccountProfileDetails = () => {
+  const [values, setValues] = useState({
+    firstName: 'neok',
+    lastName: 'dev',
+    email: 'demo@neokhub.com',
+    phone: '',
+    state: 'mon-mgl',
+    country: 'mgl'
+  });
+
+  const handleChange = useCallback(
+    (event) => {
+      setValues((prevState) => ({
+        ...prevState,
+        [event.target.name]: event.target.value
+      }));
+    },
+    []
+  );
+
+  const handleSubmit = useCallback(
+    (event) => {
+      event.preventDefault();
+    },
+    []
+  );
+
+  return (
+    <form
+      autoComplete="off"
+      noValidate
+      onSubmit={handleSubmit}
+    >
+      <Card>
+        <CardHeader
+          subheader="Мэдээллээ засах хэсэг"
+          title="Нүүр Хуудас"
+        />
+        <CardContent sx={{ pt: 0 }}>
+          <Box sx={{ m: -1.5 }}>
+            <Grid
+              container
+              spacing={3}
+            >
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  helperText="Энд Эцэг Эхийн нэрээ бичнэ үү!"
+                  label="Эцэг эхийн нэр"
+                  name="firstName"
+                  onChange={handleChange}
+                  required
+                  value={values.firstName}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Өөрийн Нэр"
+                  name="lastName"
+                  onChange={handleChange}
+                  required
+                  value={values.lastName}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Имайл Хаяг"
+                  name="email"
+                  onChange={handleChange}
+                  required
+                  value={values.email}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Утасны Дугаар"
+                  name="phone"
+                  onChange={handleChange}
+                  type="number"
+                  value={values.phone}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Улс"
+                  name="country"
+                  onChange={handleChange}
+                  required
+                  value={values.country}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Дүүрэг сонгох"
+                  name="state"
+                  onChange={handleChange}
+                  required
+                  select
+                  SelectProps={{ native: true }}
+                  value={values.state}
+                >
+                  {states.map((option) => (
+                    <option
+                      key={option.value}
+                      value={option.value}
+                    >
+                      {option.label}
+                    </option>
+                  ))}
+                </TextField>
+              </Grid>
+            </Grid>
+          </Box>
+        </CardContent>
+        <Divider />
+        <CardActions sx={{ justifyContent: 'flex-end' }}>
+          <Button variant="contained">
+            Save details
+          </Button>
+        </CardActions>
+      </Card>
+    </form>
+  );
+};
